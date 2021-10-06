c1ffd19e2ecdb46123f832d9d917314390254b3c
Klova <nets228322@gmail.com>
2021-10-06 10:29:13 +0300

    asd

---
+++ Documents/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents
+{
+    class User
+    {
+        public string Email { get; set;}
+
+        public string Role { get; set; }
+
+        public User(string email, string role)
+        {
+            Email = email;
+            Role = role;
+        }
+    }
+}
---
+++ Documents/SuperAdmin.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Documents
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class SuperAdmin : Page
+    {
+        public SuperAdmin()
+        {
+            this.InitializeComponent();
+            UserList.Items.Add(new User("test@gmail.com", "test"));
+            UserList.Items.Add(new User("test@gmail.com", "test"));
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Frame.Navigate(typeof(UserSettings));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
