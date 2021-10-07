7d5694f35acf34c1bf05f10134ce0f3a45d34582
Philipp <isip_f.v.bashkir@mpt.ru>
2021-10-07 12:23:54 +0300

    Переверстано окно с пользователями

--- Documents/Xaml/Admin/SuperAdmin.xaml.cs
+++ Documents/Xaml/Admin/SuperAdmin.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -33,14 +21,15 @@ namespace Documents
             
         }
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            Frame.Navigate(typeof(UserSettings));
-        }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             
+        }
+
+        private void UserList_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Frame.Navigate(typeof(UserSettings));
         }
     }
 }
