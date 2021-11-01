77631d2fe22f08e93d1ff96ced19f697738f4a79
Philipp <isip_f.v.bashkir@mpt.ru>
2021-11-01 09:21:49 +0300

    Мелкие исправления

--- Documents/Moduls/ApiWork.cs
+++ Documents/Moduls/ApiWork.cs
@@ -10,12 +10,12 @@ using Newtonsoft.Json;
 
 namespace Documents.Moduls
 {
-     class ApiWork
+     static class ApiWork
      {
         private static string baseUrl = "http:/";
 
 
-        public async Task<List<Document>> GetAllDocuments(int personId)
+        public static Task<List<Document>> GetAllDocuments(int personId)
         {
             var response = await $@"{baseUrl}".AppendPathSegment("/documents").GetStringAsync();
             
@@ -24,7 +24,7 @@ namespace Documents.Moduls
             return documents;
         }
 
-        public async Task<List<User>> GetAllUsers()
+        public static Task<List<User>> GetAllUsers()
         {
             var response = await $@"{baseUrl}".AppendPathSegment("/users").GetStringAsync();
 
@@ -33,7 +33,7 @@ namespace Documents.Moduls
             return documents;
         }
         
-        public async Task<List<Template>> GetAllTemplates()
+        public static Task<List<Template>> GetAllTemplates()
         {
             var response = await $@"{baseUrl}".AppendPathSegment("/templates").GetStringAsync();
 
@@ -42,7 +42,7 @@ namespace Documents.Moduls
             return documents;
         }
 
-        public async Task<List<Role>> GetAllRoles()
+        public static Task<List<Role>> GetAllRoles()
         {
             var response = await $@"{baseUrl}".AppendPathSegment("/roles").GetStringAsync();
 
@@ -51,37 +51,37 @@ namespace Documents.Moduls
             return documents;
         }
 
-        public async void UpdateUser(string newEmail,string newFullName, string newRole) 
+        public static void UpdateUser(string newEmail,string newFullName, string newRole) 
         {
             var user = new User(newEmail,newFullName,newRole);
             var response = await $"{baseUrl}".AppendPathSegment("/update_user").PutJsonAsync(user);
         }
 
-        public async void UpdateDocument(string newName,string newDescription, Template newTemplate, byte newFile)
+        public static void UpdateDocument(string newName,string newDescription, Template newTemplate, byte newFile)
         {
             var document = new Document(newName, newDescription, newTemplate, newFile);
             var response = await $"{baseUrl}".AppendPathSegment("/update_document").PutJsonAsync(document);
         }
 
-        public async void UpdateRole(string name, int count)
+        public static void UpdateRole(string name, int count)
         {
             var role = new Role(name, count);
             var response = await $"{baseUrl}".AppendPathSegment("/update_role").PutJsonAsync(role);
         }
 
-        public async void CreateRole(string name,int count)
+        public static void CreateRole(string name,int count)
         {
             var role = new Role(name, count);
             var response = await $"{baseUrl}".AppendPathSegment("/create_role").PostJsonAsync(role);
         }
 
-        public async void CreateDocument(string name, string description, Template template, byte file)
+        public static void CreateDocument(string name, string description, Template template, byte file)
         {
             var document = new Document(name, description, template, file);
             var response = await $"{baseUrl}".AppendPathSegment("/create_document").PutJsonAsync(document);
         }
 
-        public async void CreateTemplate(string name, int count)
+        public static void CreateTemplate(string name, int count)
         {
             var template = new Template(name, count);
             var response = await $"{baseUrl}".AppendPathSegment("/create_template").PutJsonAsync(template);
